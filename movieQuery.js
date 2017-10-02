e681855be43c2e666c1dd706fc7548b4491910a7
Nazmul <1430261@iub.edu.bd>
2017-10-02 09:14:58 +0600

    task1,2,3

--- movieQuery.js
+++ movieQuery.js
@@ -4,20 +4,22 @@ var movieList = [
   { title: "Omen : The Final Conflict", year: "1981", length: 108, rating: "6.7", genre: "horror" },
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
-  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" }
+  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" },
+  { title: "Terminator 2", year: "1991",length :137,rating:"8.5",genre: "action"},
 ];
 
 function getMovieTitle( index ){
-  //add code
+  return movieList[index].title;
 }
 
 console.log(getMovieTitle(3));
 
 function addMovie( movie ){
-  //add code
+  movieList.push(movie);
+  return movieList;
 }
 
-var newMovie = {};
+var newMovie = {title: "Terminator 2",year: "1991",length :137,rating:"8.5",genre: "action"};
 console.log(addMovie(newMovie));
 
 function movieByRating(){
